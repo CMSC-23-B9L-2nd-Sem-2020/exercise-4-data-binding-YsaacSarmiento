0dc70cf9e0209790ebc453e37834940732f3fe9b
YsaacSarmiento <rmsarmiento5@up.edu.ph>
2020-03-06 23:28:55 +0800

    first commit

---
+++ app/src/main/java/com/example/sarmiento_exer4_slambook/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.sarmiento_exer4_slambook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.sarmiento_exer4_slambook.databinding.ActivityMainBinding
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
